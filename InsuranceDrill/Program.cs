3b62bad760076e4d67fc95854b07a3d187fe0c0d
WA <45290206+wgreen80021@users.noreply.github.com>
2019-02-27 18:39:41 -0700

    Add files via upload

---
+++ InsuranceDrill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InsuranceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string Age = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have? ");
+            string TIX = Console.ReadLine();
+            string DUIupp = DUI.ToUpper();
+
+            bool Insurance = (Convert.ToInt32(Age) > 15 && DUIupp == "NO" && Convert.ToInt32(TIX) <= 3);
+
+            Console.WriteLine("Do you qualify for insurance ?  ");
+            Console.WriteLine(Insurance);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
